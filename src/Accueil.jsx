2b7ec264bd4e48dd8c8ce897d41034f85271d2ce
essonebiyoghecharly-ui <essonebiyoghecharly@gmail.com>
2026-09-13 22:20:37 +0100

    Delete src/Accueil.jsx

--- src/Accueil.jsx
+++
@@ -1,8 +0,0 @@
-export default function Accueil() {
-  return (
-    <div style={{padding: '20px', textAlign: 'center'}}>
-      <h2>Bienvenue sur LabGabon V6</h2>
-      <p>Le labo virtuel du Gabon</p>
-    </div>
-  )
-}
