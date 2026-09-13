6e4da7d4c552123f92e9f1bb1ca6ff15e1536ea1
essonebiyoghecharly-ui <essonebiyoghecharly@gmail.com>
2026-09-13 20:48:39 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{textAlign: 'center', padding: '50px', fontFamily: 'Arial'}}>
+      <h1>LabGabon V6</h1>
+      <p>Laboratoire intelligent</p>
+    </div>
+  )
+}
